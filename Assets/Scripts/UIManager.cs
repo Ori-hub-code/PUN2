b055dc5f2db2fca7015c624c9b83fc3ab89b5d59
Ori <uic3883@gmail.com>
2024-04-27 22:28:21 +0900

    세팅

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    InputField userIDInput;
+
+    public void StartButton()
+    {
+
+    }
+}
