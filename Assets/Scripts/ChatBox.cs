49348e5a164375187afa5334da7a567d105263b6
Ori <uic3883@gmail.com>
2024-05-02 17:20:44 +0900

    본인 채팅, 상대 채팅 위치 조정

--- Assets/Scripts/ChatBox.cs
+++ Assets/Scripts/ChatBox.cs
@@ -2,6 +2,7 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,32 +10,45 @@ public class ChatBox : MonoBehaviour
 {
     UIManager uiManager;
     public PhotonView pv;
-    Image img; // 배경 이미지
+    public GameObject realChatBox;
     public TextMeshProUGUI chatText; // 텍스트
+    VerticalLayoutGroup verticalLayoutGroup;
+    Image chatBackImage;
 
     private void Awake()
     {
         uiManager = UIManager.instance;
         pv = GetComponent<PhotonView>();
-        img = GetComponent<Image>();
+        verticalLayoutGroup = GetComponent<VerticalLayoutGroup>();
+
+        chatBackImage = realChatBox.GetComponent<Image>();
     }
 
     private void OnEnable()
     {
         transform.SetParent(uiManager.textBoxParent);
 
-        if (pv.IsMine)
+        if(pv.IsMine)
         {
+            verticalLayoutGroup.childAlignment = TextAnchor.UpperRight;
+            chatBackImage.color = Color.yellow;
+
             pv.RPC("SetText", RpcTarget.AllBuffered, uiManager.textInput.text);
-            uiManager.textInput.text = ""; // input field 초기화
-            img.color = Color.yellow;
+        } 
+        else
+        {
+            verticalLayoutGroup.childAlignment = TextAnchor.UpperLeft;
+            chatBackImage.color = Color.gray;
         }
+
+        //pv.RPC("SetText", RpcTarget.AllBuffered, uiManager.textInput.text);
+        uiManager.textInput.text = ""; // input field 초기화
     }
 
     [PunRPC]
     void SetText(string text)
     {
-        chatText.text = ""; // 초기화
+        this.chatText.text = ""; // 초기화
 
         // 줄바꿈
         char[] textWords = text.ToCharArray();
